f7ea9d5d541c9083da32d8019b59f3a0dd705194
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-09-06 21:04:17 +0100

    Gate sysproto module to macOS
    The `SYSPROTO_EVENT` and `SYSPROTO_CONTROL` constants are only available
    on macOS, see https://github.com/rust-lang/libc/pull/5158.

--- src/net/types.rs
+++ src/net/types.rs
@@ -1116,6 +1116,7 @@ pub mod ipproto {
 }
 
 /// `SYSPROTO_*` constants.
+#[cfg(target_os = "macos")]
 pub mod sysproto {
     #[cfg(apple)]
     use {
@@ -1124,11 +1125,9 @@ pub mod sysproto {
     };
 
     /// `SYSPROTO_EVENT`
-    #[cfg(apple)]
     pub const EVENT: Protocol = Protocol(new_raw_protocol(c::SYSPROTO_EVENT as _));
 
     /// `SYSPROTO_CONTROL`
-    #[cfg(apple)]
     pub const CONTROL: Protocol = Protocol(new_raw_protocol(c::SYSPROTO_CONTROL as _));
 }
 
